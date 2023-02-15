ec5fd4b8aaa3667dd11bcf3e63a43cb7a991bef1
Agustin Kassis <webmaster@masize.com>
2023-02-15 18:24:27 -0300

    fixed logo padding

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,7 +4,7 @@ import LogoSvg from "../public/logo.svg";
 const Logo = () => {
   return (
     <div
-      className='absolute top-0 left-[50%] translate-x-[-50%] translate-y-[-50%] px-5 pt-4 pb-6
+      className='absolute top-0 left-[50%] translate-x-[-50%] translate-y-[-50%]
       bg-white rounded-full ring-2 ring-gray-900/5
       shadow-lg
     '
